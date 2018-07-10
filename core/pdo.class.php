36c07222851208e59ae18992396bd631e30394b1
Rati <ratibysent@gmail.com>
2018-07-10 16:26:22 +0400

    ბოლო ცვლილებაბაზის დამატება

--- core/pdo.class.php
+++ core/pdo.class.php
@@ -2,8 +2,17 @@
 Namespace RuParser; 
 Class mydb extends PDO
 {
-    public function __contruct(){
-        
+    public function __contruct($file = __DIR__ . '/../pdo.ini'){
+        if (!$settings = parse_ini_file($file, TRUE)) 
+            throw new exception('Unable to open ' . $file . '.');
+        $dns = $settings['database']['driver'] .
+        ':host=' . $settings['database']['host'] .
+        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+        ';dbname=' . $settings['database']['schema'];
+
+        parent::__construct($dns, $settings['database']['username'],$settings['database']['password']);
+
+
     }
 }
 ?>
